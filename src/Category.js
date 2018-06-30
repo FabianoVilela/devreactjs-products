890246b1c91af36df6eb3b8724d3ad0a91255bee
Fabiano Vilela <fabiano.vilela@gmail.com>
2018-06-30 18:55:34 -0300

    List products by category. Add new category

--- src/Category.js
+++ src/Category.js
@@ -1,8 +1,41 @@
-import React, { Component } from 'react'
+import React, {
+  Component
+} from 'react'
+import axios from 'axios'
 
 class Category extends Component {
+  constructor (props) {
+    super(props)
+    this.loadProducts = this.loadProducts.bind(this)
+    this.state = {
+      products: []
+    }
+  }
+  loadProducts (id) {
+    axios.get(`http://localhost:3001/products?category=${id}`)
+      .then(res => {
+        this.setState({
+          products: res.data
+        })
+      })
+  }
+  componentDidMount () {
+    let id = this.props.match.params.id
+    this.loadProducts(id)
+  }
+  componentWillReceiveProps (newProps) {
+    let id = newProps.match.params.id
+    this.loadProducts(id)
+  }
+  renderProducts (product) {
+    return (
+      <li key={product.id}>{product.name}</li>
+    )
+  }
   render () {
-    return <h2>Category {this.props.match.params.id}</h2>
+    return ( 
+      <ul>{this.state.products.map(this.renderProducts)}</ul>
+    )
   }
 }
 
--- src/Products.js
+++ src/Products.js
@@ -6,13 +6,18 @@ import ProductsHome from './ProductsHome'
 import Category from './Category'
 
 class Products extends Component {
-  constructor(props){
+  constructor (props) {
     super(props)
+    this.handlerNewCategory = this.handlerNewCategory.bind(this)
+    this.loadCategories = this.loadCategories.bind(this)
     this.state = {
       categories: []
     }
   }
-  componentDidMount() {
+  componentDidMount () {
+    this.loadCategories()
+  }
+  loadCategories () {
     axios
       .get('http://localhost:3001/categories')
       .then(res => {
@@ -21,23 +26,43 @@ class Products extends Component {
         })
       })
   }
-  renderCategory(c) {
-    return(
-      <li key={c.id}>
-        <Link to={`/products/category/${c.id}`}>{c.name}</Link>
+  renderCategory (category) {
+    return (
+      <li key={category.id} className='list-group-item'>
+        <Link to={`/products/category/${category.id}`}>{category.name}</Link>
       </li>
     )
+  }
+  setActive () {
+    return 'active'
+  }
+  handlerNewCategory (key) {
+    if (key.keyCode === 13) {
+      axios
+        .post('http://localhost:3001/categories',
+          {
+            name: this.refs.category.value
+          })
+        .then(res => {
+          this.loadCategories()
+          this.refs.category.value = ''
+        })
+    }
   }
   render () {
     const { match } = this.props
     const { categories } = this.state
     return (
       <div className='row'>
-        <div className='col-md-2'>
+        <div className='col-md-3'>
           <h3>Categories</h3>
-          <ul>{categories.map(this.renderCategory)}</ul>
+          <div className='form-group'>
+            <label>Add category</label>
+            <input className='form-control' type='text' ref='category' placeholder='New category' onKeyUp={this.handlerNewCategory} />
+          </div>
+          <ul className='list-group'>{categories.map(this.renderCategory)}</ul>
         </div>
-        <div className='col-md-10'>
+        <div className='col-md-9'>
           <h1>Products</h1>
           <Route exact path={match.url} component={ProductsHome} />
           <Route path={match.url + '/category/:id'} component={Category} />
